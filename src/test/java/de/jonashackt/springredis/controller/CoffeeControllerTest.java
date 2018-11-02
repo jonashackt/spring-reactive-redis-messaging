2b28f600794ed86706d0019321aaf34e36062bfd
Jonas Hecht <jonas.hecht@codecentric.de>
2018-11-02 11:03:49 +0100

    Working solution - removed Dockerfile, since we only need redis for testing and prod

--- src/test/java/de/jonashackt/springredis/controller/CoffeeControllerTest.java
+++ src/test/java/de/jonashackt/springredis/controller/CoffeeControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
@@ -16,24 +17,30 @@ import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.containsString;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringredisApplication.class)
+@SpringBootTest(
+        classes = SpringredisApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CoffeeControllerTest {
+
+    @LocalServerPort
+    private int port;
 
     @ClassRule
     public static DockerComposeContainer services =
-            new DockerComposeContainer(new File("../docker-compose.yml"))
+            new DockerComposeContainer(new File("docker-compose.yml"))
                     .withExposedService("redis", 6379, Wait.forListeningPort());
-                    //.withExposedService("springredis", 8080, Wait.forHttp("/coffees").forStatusCode(200));
 
 
     @Test
     public void does_coffee_work_with_redis() {
 
         when()
-            .get("http://localhost:8080/coffees")
+            .get("http://localhost:" + port + "/coffees")
         .then()
             .statusCode(HttpStatus.SC_OK)
             .assertThat()
-                .body(containsString("Both Servers called - Alice said 'Alice answering!' & Tom replied 'Tom answering!'."));
+                .body(containsString("Black Alert Redis"))
+                .body(containsString("Darth Redis"))
+                .body(containsString("Jet Black Redis"));
     }
 }
